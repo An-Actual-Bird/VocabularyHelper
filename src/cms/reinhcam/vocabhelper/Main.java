1fa89438a90823431ed74b350737c8c53b1a6a5e
An-Actual-Bird <16reinhcam@gmail.com>
2014-11-24 23:40:24 -0500

    Touched up, somewhat functional, works with the first URL I tried, the wiki page for 'Government', doesn't work for 'Congress', yet.

--- src/cms/reinhcam/vocabhelper/Main.java
+++ src/cms/reinhcam/vocabhelper/Main.java
@@ -6,13 +6,14 @@ import java.net.URL;
 
 public class Main
 {
-    static String string;
+    static String end = "";
+
     public static void main(String[] args) throws Exception
     {
 
-        URL oracle = new URL("https://simple.wikipedia.org/wiki/Government");
+        URL wikipedia = new URL("https://simple.wikipedia.org/wiki/Congress");
         BufferedReader in = new BufferedReader(
-                new InputStreamReader(oracle.openStream()));
+                new InputStreamReader(wikipedia.openStream()));
 
         String inputLine;
         while ((inputLine = in.readLine()) != null)
@@ -22,24 +23,41 @@ public class Main
                 if (inputLine.charAt(0) == '<' && inputLine.charAt(1) == 'p' && inputLine.charAt(2) == '>')
                 {
                     replace(inputLine);
-                    string = inputLine;
-                    System.out.println(inputLine);
+                    break;
                 }
-            }catch(Exception e){}
+            } catch (Exception e)
+            {
+            }
         }
         in.close();
     }
-    public static String replace(String string)
-    {
-        string = string.replaceAll("<p>","");
-        string = string.replaceAll("<b>","");
-        string = string.replaceAll("</b>","");
-        string = string.replaceAll("<a href=\"/wiki/","");
-        string = string.replaceAll("title=\"","");
-        string = string.replaceAll("</a>","");
-        string = string.replaceAll("</p>","");
-        string = string.replaceAll(" ","");
 
-        return string;
+    public static void replace(String string)
+    {
+        string = string.replaceAll("<p>", "");
+        string = string.replaceAll("<b>", "");
+        string = string.replaceAll("</b>", "");
+        string = string.replaceAll("<a href=\"/wiki/", "");
+        string = string.replaceAll("title=\"", "");
+        string = string.replaceAll("</a>", "");
+        string = string.replaceAll("</p>", "");
+        string = string.replaceAll(">", " ");
+
+        String[] trim = string.split(" ");
+        for (int i = 0; i < trim.length; i++)
+        {
+            if (i < trim.length - 1)
+            {
+                if (trim[i].equals(trim[i + 1]))
+                {
+                    trim[i] = "";
+                    trim[i + 1] = "";
+
+                    i++;
+                }
+            }
+            end = end + (trim[i] + " ");
+        }
+        System.out.println(end);
     }
 }
